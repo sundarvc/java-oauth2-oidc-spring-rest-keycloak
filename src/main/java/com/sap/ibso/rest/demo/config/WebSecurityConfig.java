750bb2505ff6a50d46b43407437414599af1f4bf
i821932 <sundar.chakravarthy@sap.com>
2020-10-07 11:45:07 -0400

    Checking for authentication

--- src/main/java/com/sap/ibso/rest/demo/config/WebSecurityConfig.java
+++ src/main/java/com/sap/ibso/rest/demo/config/WebSecurityConfig.java
@@ -10,15 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import com.sap.ibso.rest.demo.user.LoadUserTable;
 import com.sap.ibso.rest.demo.user.User;
 import com.sap.ibso.rest.demo.user.UserAuthority;
 import com.sap.ibso.rest.demo.user.UserRepository;
@@ -30,21 +31,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests(a -> {
-			a.mvcMatchers(HttpMethod.GET, "/products/**").hasAuthority("READ");
-			a.anyRequest().hasAuthority("WRITE");
+			a.mvcMatchers(HttpMethod.GET, "/products/**").hasAuthority("READ").
+			mvcMatchers(HttpMethod.PUT, "/products/**").hasAuthority("WRITE").
+			anyRequest().authenticated();
 		})
 		.httpBasic();
-
-
-		
 		http.csrf().disable();
 
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
 	
 	@Bean
