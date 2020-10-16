7e2471c76b8d2a3b0c063c37f044b61770353c00
i821932 <sundar.chakravarthy@sap.com>
2020-10-15 20:09:04 -0400

    Added deployment file

--- src/main/java/com/sap/ibso/rest/demo/web/ProductController.java
+++ src/main/java/com/sap/ibso/rest/demo/web/ProductController.java
@@ -3,6 +3,8 @@ package com.sap.ibso.rest.demo.web;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sap.ibso.rest.demo.model.Product;
 import com.sap.ibso.rest.demo.model.ProductRepository;
 import com.sap.ibso.rest.demo.model.exceptions.ProductNotFoundException;
+import com.sap.ibso.rest.demo.user.LoadUserTable;
 
 @RestController
 class ProductController {
+	
+  private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
   private final ProductRepository repository;
 
@@ -28,6 +33,7 @@ class ProductController {
 
   @GetMapping("/products")
   List<Product> all() {
+	log.info("Retrieving all products");  
     return repository.findAll();
   }
 
@@ -40,14 +46,13 @@ class ProductController {
 
   @GetMapping("/products/{id}")
   Product one(@PathVariable Long id) {
-
+	log.info("Retrieving product " + id);
     return repository.findById(id)
       .orElseThrow(() -> new ProductNotFoundException(id));
   }
 
   @PutMapping("/products/{id}")
   Product replaceProduct(@RequestBody Product newProduct, @PathVariable Long id) {
-
     return repository.findById(id)
       .map(product -> {
     	  product.setName(newProduct.getName());
